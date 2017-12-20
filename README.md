0b145775a6bdc4a9c305646ea927b66777884b8f
Lex Martinez <lex0316@gmail.com>
2017-12-19 19:09:06 -0500

    :satellite:

---
+++ README.md
@@ -0,0 +1,31 @@
+# ip-geocoder
+Find geocoding information from IP address
+Powered by [ipapi.co](https://ipapi.co)
+
+## Install
+
+```bash
+$ npm install --global ip-geocoder
+```
+
+
+## Usage
+
+```bash
+
+  Usage
+    $ ip-geocoder 8.8.8.8
+    
+      ip-geocoder
+      Find geocoding information from IP address
+      Powered by ipapi.co
+      
+      ip:           8.8.8.8
+      .......
+
+```
+
+
+## License
+
+This project is licensed under MIT License - see the [LICENSE.md](https://github.com/lexmartinez/ip-geocoder/blob/master/LICENSE.md) file for details
---
+++ cli.js
@@ -0,0 +1,37 @@
+'use strict'
+const chalk = require('chalk')
+const log = console.log
+const args = process.argv
+const ip = args[2]
+const ipRegex = require('ip-regex')
+const request = require('request')
+const ora = require('ora')
+const prettyjson = require('prettyjson')
+
+const usage = () => {
+  log(chalk.green.underline.bold('Usage'))
+  log(chalk`  $ ip-geocoder {yellow 8.8.8.8}`)
+}
+
+log('\n' + chalk.blue.underline.bold('ip-geocoder'))
+log('Find geocoding information from IP address')
+log(chalk`Powered by {underline ipapi.co}\n`)
+
+if (ip) {
+  if (ipRegex.v4({exact: true}).test(ip)) {
+    const spinner = ora(`Geocoding IP: ${ip}`).start()
+    request(`https://ipapi.co/${ip}/json`, (error, response, body) => {
+      spinner.stop()
+      if (error) {
+        log(chalk.red(chalk`{bold Error} ${error}`))
+      } else {
+        log(prettyjson.render(JSON.parse(body)))
+      }
+    })
+  } else {
+    log(chalk.red(chalk`{bold Error} Invalid IP address input`))
+    usage()
+  }
+} else {
+  usage()
+}
